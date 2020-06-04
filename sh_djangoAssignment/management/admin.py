ef848780a40fbf522551fc0fd90b4a3fcab38203
HiraAltaf <hiraaltaf086@gmail.com>
2020-06-04 17:43:41 +0500

    Removed old directory

--- sh_djangoAssignment/management/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MalfunctionReport,ImprovementReport,InvestigateImprovement,InvestigateMalfunction
-# Register your models here.
-admin.site.register(MalfunctionReport)
-admin.site.register(ImprovementReport)
-admin.site.register(InvestigateImprovement)
-admin.site.register(InvestigateMalfunction)
-
